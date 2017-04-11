009d171e4614ffe87155e2524dc613574c312c32
infernosquad <infzanoza@gmail.com>
2017-04-11 14:25:10 +0500

    Initial commit

---
+++ src/AppBundle/Controller/HomepageController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+
+class HomepageController extends Controller
+{
+    public function indexAction(Request $request)
+    {
+        return $this->render('AppBundle:Homepage:index.html.twig');
+    }
+}
